313c9f01e9012d6b1640f1f86ba3ac6e24e8bbfb
Gabrielfs97 <81993925+Gabrielfs97@users.noreply.github.com>
2025-09-01 10:44:38 -0300

    exercicio 02 de lista

---
+++ modulo_2/Aula01/exe02.py
@@ -0,0 +1,19 @@
+
+lista_a = ["azul", "verde", "amarelo", "vermelho", "branco", "preto"]
+
+lista_b = ["roxo", "azul", "verde", "laranja", "preto", "vermelho"]
+
+lista_c = []
+
+itens = 0
+
+for i in lista_a:
+    for b in lista_b:
+        if i == b:
+            lista_c.append(i)
+            itens += 1
+
+print(lista_a)
+print(lista_b)
+print(f"foram encontrados {itens} itens iguais nas duas listas")
+print(' '.join(lista_c))
