407e5fcc0722dca53bbc307b24f728585c86ccbf
Gabrielfs97 <81993925+Gabrielfs97@users.noreply.github.com>
2025-09-01 11:50:09 -0300

    exe04 terminar em casa

---
+++ modulo_2/Aula01/exe04.py
@@ -0,0 +1,10 @@
+lista_b = []
+
+while True:
+ 
+ lista = input("Digite uma lista de números separados por vírgula: ")
+ if lista:
+     lista.append(lista_b)
+ elif -1 in lista:
+        break
+ print(lista_b)
