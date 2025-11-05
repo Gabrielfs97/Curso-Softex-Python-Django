82c1dd1e18eb836bdaeccd98b4998b2711363556
M4rib4ck <81993925+Gabrielfs97@users.noreply.github.com>
2025-11-05 11:37:25 -0300

    site update

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
      
 
 def home(request): 
 # Vamos retornar a resposta HTTP mais simples: um texto HTML 
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = {
+'nome_usuario': 'Gabriel',
+'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+}
+    return render(request, 'home.html', context)
+
+
 
 def mainPage(request):
     return HttpResponse("<h2>Catapimbas, segundo olá, mundo!")
