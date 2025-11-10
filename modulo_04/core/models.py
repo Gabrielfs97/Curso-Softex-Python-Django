a324bfdd8cc16e48a2183579755309204f3ee922
M4rib4ck <81993925+Gabrielfs97@users.noreply.github.com>
2025-11-10 11:39:49 -0300

    subindo teste site_django com superuser

--- modulo_04/core/models.py
+++ modulo_04/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+
+    titulo = models.CharField(max_length=200)
+
+    # Um campo booleano (verdadeiro/falso), que por padrão é Falso
+    concluida = models.BooleanField(default=False)
+    # Um campo de data e hora.
+    # auto_now_add=True: Salva a data e hora EXATAMENTE quando o objeto é criado
+    criada_em = models.DateTimeField(auto_now_add=True)
+    # Este é um "método mágico" do Python.
+    # Ele diz ao Django como "chamar" um objeto Tarefa.
+    # Em vez de "Tarefa object (1)", ele mostrará o título da tarefa.
+    # Isso é EXTREMAMENTE útil no painel de administração.
+    def __str__(self):
+        return self.titulo
+
+class Teste(models.Model):
+
+    nome = models.CharField(max_length=200)
+
+    local = models.CharField(max_length=20)
+
+    hora = models.DateTimeField(auto_now_add=True)
+
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Tarefa
+from .models import Teste
 # Create your views here.
 
      
 
 def home(request): 
+    todas_as_tarefas = Tarefa.objects.all()
+    teste_teste = Teste.objects.all()
 # Vamos retornar a resposta HTTP mais simples: um texto HTML 
     context = {
 'nome_usuario': 'Gabriel',
-'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+'tecnologias': ['Python', 'Django','CSS', 'Models', 'Admin',],
+'tarefas': todas_as_tarefas,
+'teste': teste_teste
 }
     return render(request, 'home.html', context)
 
